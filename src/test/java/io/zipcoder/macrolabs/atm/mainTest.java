3c9996883ae6b7257ea83cf6c4334494faeaecf5
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-10-24 15:00:59 -0400

    Create unit test for Security.getNumberOwned()

--- src/test/java/io/zipcoder/macrolabs/atm/mainTest.java
+++ src/test/java/io/zipcoder/macrolabs/atm/mainTest.java
@@ -16,6 +16,15 @@ public class mainTest {
 
         Assert.assertEquals("The strings do not match", expected, actual);
     }
+    @Test
+    public void testSecurityGetNumberOwned(){
+        double expected=10.030;
+        Security security = new Security("XKCD", expected, 25.12);
+        double actual = security.getNumberOwned();
+
+        Assert.assertEquals(expected, actual, 0.0001);//Seeking reasonable precision within
+                                                    // three decimal places
+    }
 
 
 }
