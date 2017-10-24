759ec1ce3d06e2cca2b56d691f28750e8cde8a1c
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-10-24 14:13:39 -0400

    Create project and update to include jUnit functionality

---
+++ src/test/java/io/zipcoder/macrolabs/atm/mainTest.java
@@ -0,0 +1,16 @@
+package io.zipcoder.macrolabs.atm; /**
+ * Created by Timothy Rager on 10/24/17.
+ */
+import org.junit.Assert;
+import org.junit.Test;
+
+public class mainTest {
+
+
+    @Test
+    public void testMethod(){
+
+    }
+
+
+}
