ea1f497012bc5b3297699e40f00099eb4fc18ce0
Ryan <ryan@rmiller.dev>
2019-10-26 17:23:02 -0400

    overide toStringArray

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -2,11 +2,11 @@ import java.util.Date;
 
 public class Transaction implements Storeable {
 
-    public Double amount;
-    public Date timeStamp;
-    public Integer accountID;
-    public String description;
-    public Boolean isCredit;
+    private Double amount;
+    private Date timeStamp;
+    private Integer accountID;
+    private String description;
+    private Boolean isCredit;
 
     public Transaction(Double amount, Date timeStamp, Integer accountID, String description, Boolean isCredit) {
         this.amount = amount;
@@ -14,5 +14,29 @@ public class Transaction implements Storeable {
         this.accountID = accountID;
         this.description = description;
         this.isCredit = isCredit;
+    }
+
+    public String print() {
+        return amount + " " + description + timeStamp;
+    }
+
+
+    @Override
+    public String[] toStringArray() {
+        String type;
+        if (isCredit) {
+            type = "credit";
+        } else {
+            type = "debit";
+        }
+
+        String[] result = new String[] {
+                type,
+                this.accountID.toString(),
+                this.amount.toString(),
+                this.timeStamp.toString(),
+                this.description
+        };
+        return result;
     }
 }
