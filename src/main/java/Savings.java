cc07aa958d6ca96db4743fc44bc4233e7d6df791
Mike King <michkin32@gmail.com>
2019-10-26 16:36:50 -0400

    added fields

--- src/main/java/Savings.java
+++ src/main/java/Savings.java
@@ -6,33 +6,5 @@ public class Savings extends Account{
         this.interestRate = interestRate;
     }
 
-    @Override
-    public Double getBalance() {
-        return super.getBalance();
-    }
 
-    @Override
-    public void transferToAnother() {
-        super.transferToAnother();
-    }
-
-    @Override
-    public void transferWithin() {
-        super.transferWithin();
-    }
-
-    @Override
-    public void withdraw(Double amount) {
-        super.withdraw(amount);
-    }
-
-    @Override
-    public void getAcctHist() {
-        super.getAcctHist();
-    }
-
-    @Override
-    public void deposit(Double amount) {
-        super.deposit(amount);
-    }
 }
