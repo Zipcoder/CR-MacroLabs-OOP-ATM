2717f9a319404df60c96cca27aca36d0b5efcc9a
Ryan <ryan@rmiller.dev>
2019-10-27 11:54:36 -0400

    Card Number Gen

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,5 +1,7 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -11,5 +13,18 @@ public class UserTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void genCardNumTest() {
+
+        System.out.println("Testing Card Number Gen");
+
+        for (int i = 0; i < 10; i++) {
+            Integer cardNum = User.genCardNum();
+
+            System.out.println(cardNum);
+            Assert.assertEquals(8,  cardNum.toString().length());
+        }
     }
 }
