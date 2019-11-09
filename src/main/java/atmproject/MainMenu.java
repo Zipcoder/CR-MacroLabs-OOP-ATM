1220fb2bb693eb101d3188ae97aeb6c4c10dd8f1
Christopher Santana <chris@zipcoders-MacBook-Pro-6.local>
2019-11-09 18:05:05 -0500

    savinf progress on main menu

--- src/main/java/atmproject/MainMenu.java
+++ src/main/java/atmproject/MainMenu.java
@@ -1,48 +1,119 @@
 package atmproject;
 
 
-public class MainMenu  {
-    //private Console console = new Console(System.in, System.out);
+import atmproject.accounts.Account;
+
+public class MainMenu {
+    private Console console = new Console(System.in, System.out);
     private User currentUser;
+
+
+
     private boolean running = true;
+    private AccountMenu accountMenu = new AccountMenu();
 
 
-//
-//public void runMainMenu(User currentUser){
-//    this.currentUser = currentUser;
-//    while (running) {
-//
+    public void runMainMenu(User currentUser) {
+       setCurrentUser(currentUser);
+        while (running) {
+            console.println("please select your option");
+            console.println("(1) - Deposit");
+            console.println("(2) - Withdraw");
+            console.println("(3) - Transfer");
+            console.println("(4) - Balance");
+            console.println("(5) - View History");
+            console.println("(6) - Return To Login Menu");
+            Integer userInput = getUserInput();
+            mainMenuLogic(userInput);
+        }
 
- public void callDeposit() {
+
+    }
+    private Double getUserDoubleInput(){
+        return console.getDoubleInput(":");
+    }
+    private Integer getUserInput(){
+        return console.getIntegerInput(":");
+    }
+
+    public void callDeposit() {
+        Account chosen;
+        Double userInput;
+        chosen = accountMenu.selectAccount(currentUser);
+
 
     }
 
-public void callWithdraw() {
+    public void callWithdraw() {
 
-}
+    }
 
-public void callTransfer() {
+    public void callTransfer() {
 
-}
+    }
 
-public void checkBalance() {
+    public void checkBalance() {
 
-}
+    }
 
-public void displayHistory() {
+    public void displayHistory() {
 
-}
+    }
 
-public void returnTOLoginMenu() {
+    public void returnTOLoginMenu() {
+        running = false;
 
     }
 
 
+
+    public void mainMenuLogic(Integer userInput){
+
+        switch (userInput) {
+            case 1:
+                callDeposit();
+                break;
+            case 2:
+                callWithdraw();
+                break;
+            case 3:
+                callTransfer();
+                break;
+            case 4:
+                checkBalance();
+                break;
+            case 5:
+                displayHistory();
+                break;
+            case 6:
+                returnTOLoginMenu();
+                break;
+        }
+
+
+
+
+
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+
 }
-
-
-
-
 
 
 
---
+++ src/test/java/atmproject/MainMenuTest.java
@@ -0,0 +1,55 @@
+package atmproject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainMenuTest {
+
+    @Test
+    public void runMainMenu() {
+    }
+
+    @Test
+    public void callDeposit() {
+    }
+
+    @Test
+    public void callWithdraw() {
+    }
+
+    @Test
+    public void callTransfer() {
+    }
+
+    @Test
+    public void checkBalance() {
+    }
+
+    @Test
+    public void displayHistory() {
+    }
+
+    @Test
+    public void returnTOLoginMenu() {
+      MainMenu mainMenu = new MainMenu();
+
+      mainMenu.returnTOLoginMenu();
+
+      assertFalse(mainMenu.isRunning());
+
+
+    }
+
+    @Test
+    public void mainMenuLogic() {
+    }
+
+    @Test
+    public void getCurrentUser() {
+    }
+
+    @Test
+    public void setCurrentUser() {
+    }
+}
