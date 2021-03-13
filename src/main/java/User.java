d17066420781ba04a981b0f863c619d2ed5f7082
kelly <kelly@zipcodes-MacBook-Pro-9.local>
2021-03-12 22:07:39 -0500

    Initial commit: setting up User class

---
+++ src/main/java/User.java
@@ -0,0 +1,29 @@
+
+
+
+
+public class User {
+
+    public String userName;
+    private String passWord;
+    public Account[] account;
+
+
+    // default constructor
+    public User() {
+
+
+    }
+
+    //
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/test/UserTest.java
@@ -0,0 +1,9 @@
+
+
+
+
+
+
+// Test the expected User class from ATM.
+public class UserTest {
+}
